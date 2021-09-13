50a05c0272e09824fbb4927e153622996b82bfa4
Tadele01 <tadeleyednkachw@gmail.com>
2021-09-13 13:57:14 +0300

    authentication for login

--- Citizens_petitions/auth.py
+++ Citizens_petitions/auth.py
@@ -2,6 +2,7 @@ import functools
 from flask import (Blueprint, redirect, render_template, flash, g, 
                     request, session, url_for)
 from werkzeug import useragents
+from werkzeug.exceptions import RequestEntityTooLarge
 from werkzeug.security import check_password_hash, generate_password_hash
 from Citizens_petitions.db import get_db
 bp = Blueprint('auth', __name__, url_prefix='/auth')
@@ -33,4 +34,25 @@ def register():
         flash(error)
     
     return render_template('auth/register.html')
-    
+
+@bp.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        db = get_db()
+        error = None
+        user = db.execute('SELECT * FROM user WHERE username = ?', (username, )).fetchone()
+        if user is None:
+            error = "User does not exists"
+        elif not check_password_hash(user['password'], password):
+            error = "User or Password incorrect"
+        
+        if error is None:
+            session.clear()
+            session['user_id'] = user['id']
+            return redirect(url_for('index'))
+        
+        flash(error)
+
+    return render_template('auth/login.html')
